21da15ca7714f77108bf800ccc99a40a5f9e092f
arethefreshest <doffen.are@gmail.com>
2023-12-18 03:53:49 +0100

    Basic home page layouy

--- src/HomePage.js
+++ src/HomePage.js
@@ -4,7 +4,13 @@ import './css/HomePage.css';
 function HomePage() {
     return (
         <div className="home-container">
+            <img src="/pictures/logo.png" alt="Logo" className="logo" />
             <h1 className="home-title">Velkommen til vårt drikkespill!</h1>
+            <div className="buttons-container">
+                <button className="button">Start et spill</button>
+                <button className="button">Logg inn</button>
+                <button className="button">Innstillinger</button>
+            </div>
         </div>
     );
 }
